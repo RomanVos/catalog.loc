cf748aeb22397ba0382ed3a31d8182443568083d
RomanVos <68559584+RomanVos@users.noreply.github.com>
2020-08-21 16:39:32 +0300

    Update config.php

--- config.php
+++ config.php
@@ -9,5 +9,5 @@ define("PERPAGE", 5);
 
 $option_perpage = array(5,10,15,20);
 
-$connection = @mysqli_connect(DBHOST, DBUSER, DBPASS, DB) or die("Нет соединения с БД");
-mysqli_set_charset($connection, "utf8") or die("Не установлена кодировка соединения");
+$connection = @mysqli_connect(DBHOST, DBUSER, DBPASS, DB) or die("No connection to DB");
+mysqli_set_charset($connection, "utf8") or die("Connection encoding not set");
